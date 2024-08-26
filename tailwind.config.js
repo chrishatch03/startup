a462a7aeb2b6d691d528a7dd78028c50f7fcb730
Christopher Hatch <166663506+chrishatch03@users.noreply.github.com>
2024-08-26 17:00:32 -0600

    built css tile style on index.html page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Gray
+        primary: "#101010",
+        secondary: "#1a1a1a",
+        tertiary: "#262626",
+        white: "#ececec",
+      },
+      minHeight: {
+        'empty': '290px',
+      },
+    },
   },
   plugins: [],
 }
