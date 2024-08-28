dd3c4e7a563efff957f0c8e8bb0999ec9dba07c8
Christopher Hatch <166663506+chrishatch03@users.noreply.github.com>
2024-08-28 14:48:47 -0600

    installed tailwind for react

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+import './app.css';
+
+export default function App() {
+  return <div className='body bg-dark text-light'>App will display here</div>;
+}
